d8f26aea827799064beb129a02c9ae68462eac3b
anqus <angusdoh@gmail.com>
2020-10-13 10:35:51 +0100

    formatting

--- blur.py
+++ blur.py
@@ -2,10 +2,14 @@ import glob
 import cv2 as cv
 import numpy as np
 from screeninfo import get_monitors
-import ntpath
-import pathlib
+import os
 
-def blur_underlay(img, x_dim=0, y_dim=0, blur_level=70,):
+def blur_underlay(
+    img, 
+    x_dim=0, 
+    y_dim=0, 
+    blur_level=70
+    ):
 
     if x_dim==0:
         x_dim = img.shape[1]
@@ -38,14 +42,21 @@ def blur_underlay(img, x_dim=0, y_dim=0, blur_level=70,):
     bkgd_blur = cv.GaussianBlur(bkgd,ksize=(0,0),sigmaX=blur_level)
 
     # Overlay centred foreground image over background image
-    y_offset=int((bkgd_blur.shape[0]-img_resize.shape[0])/2)
-    x_offset=int((bkgd_blur.shape[1]-img_resize.shape[1])/2)
-    bkgd_blur[y_offset:y_offset+img_resize.shape[0], x_offset:x_offset+img_resize.shape[1]] = img_resize
+    y_offset = int((bkgd_blur.shape[0]-img_resize.shape[0])/2)
+    x_offset = int((bkgd_blur.shape[1]-img_resize.shape[1])/2)
+    bkgd_blur[
+        y_offset:y_offset+img_resize.shape[0], 
+        x_offset:x_offset+img_resize.shape[1]] = img_resize
 
     return bkgd_blur
 
 
-def wallpaper(input_path, output_path, monitor_num=0, filetype='.jpg'):
+def wallpaper(
+    input_path=f'{os.getcwd()}/raw_images/', 
+    output_path=f'{os.getcwd()}/processed_images/', 
+    monitor_num=0, 
+    filetype='.jpg'
+    ):
 
     # Fetch desktop resolution
     m = get_monitors()
@@ -54,21 +65,22 @@ def wallpaper(input_path, output_path, monitor_num=0, filetype='.jpg'):
 
     for input_image_path in glob.iglob(f'{input_path}*{filetype}'):
 
-        image_name=ntpath.basename(input_image_path)
-        output_image_path = f'{output_path}blurpaper_{image_name}'
-        path = pathlib.Path(output_image_path)
-        if path.exists() == True:
-            return
+        image_name = os.path.basename(input_image_path)
+        output_image_path = f'{output_path}blurpaper_{image_name}'       
+        if os.path.exists(output_image_path) == True:
+            print(f'{image_name} already processed')
+            continue
 
         # Load image and transform
         img = cv.imread(input_image_path, 1)
-        bkgd_blur = blur_underlay(img, x_dim=monitor_x_dim, y_dim=monitor_y_dim)
+        bkgd_blur = blur_underlay(
+            img, 
+            x_dim=monitor_x_dim, 
+            y_dim=monitor_y_dim
+            )
 
         # Write file
         print(output_image_path)
         cv.imwrite(output_image_path,bkgd_blur)
 
     print('Done!')
-
-
-wallpaper(input_path='C:/Users/Angus/Photos/Test/', output_path='C:/Users/Angus/Photos/Test/Blurpapers/',)
